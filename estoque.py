1a0e2f3a0b52dd7da678aeb902ac0440786a14cb
Guilherme Viana <guimeijomil@hotmail.com>
2024-11-13 11:49:49 -0300

    Adicionada Função de autenticação

--- estoque.py
+++ estoque.py
@@ -2,21 +2,48 @@ import requests
 import json
 import os
 
+# Função para obter o token de acesso
+def get_access_token():
+    token_url = 'https://api.mileexpress.com.br/oauth/token'
+    login_data = {
+        "grant_type": "password",
+        "client_id": 1,
+        "client_secret": "xwShEAUn6MJ82AZzaECmypbp6PmjTM3HPhDYaxE7",
+        "username": "veddara_integration@veddare.com.br",
+        "password": "32_s0{O)oFr!xuNy,EkY.LF",
+        "scope": "*"
+    }
+
+    response = requests.post(token_url, headers={'Content-Type': 'application/json'}, json=login_data)
+
+    if response.status_code == 200:
+        return response.json()['access_token']
+    else:
+        raise Exception(f"Erro ao obter token: {response.status_code} - {response.text}")
+
+# URL de consulta de estoque
 url = 'https://api.mileexpress.com.br/v1/warehouse/stock'
+
+# Obter token de acesso
+access_token = get_access_token()
+
+# Cabeçalhos com o token de acesso
 headers = {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOTlhYjlkZDk3YmNiYmUwMTc0YzBjMTQ0ZDA5MTliMjRjMWM0YTUzNzZhNzY2ZjhmODIwMWRkMmIxOTIxN2FkNDE3ZGZlYmFkNWZlOGNlNTMiLCJpYXQiOjE3MzE1MDU4MjcsIm5iZiI6MTczMTUwNTgyNywiZXhwIjoxNzMxNTkyMjI3LCJzdWIiOiI5MiIsInNjb3BlcyI6WyIqIl19.Ff5IwSTvOuk5Etckd2Lvx4iexnQ0twBYj2UALKLJkFo-PAjl3rexGjh4QZy9st9jsv_ZZC9TaWajTg9YsIsxeB-Xw5gM-qCo2aQc7NFpaw1IuhPuAQFfZwyDS_BwMG2tmqhaExKoexKYUD5hsh61DWUkq6TdmnhlbIDOix9WsT0txXUA1e3GTQLUGCnorgJpCuGKxNz4JVnKYBlkmynFPpTmZk2CmOg-qGa7TeNFAYVdNZ5JBbvPXbUu5b8nXcdy5pF-W33MOgbIyDleIFuVgOWEjpsep4VJBWH1wgbXcs3B5ectHxcB2roYBHTh5DtSx69j4N9Sz9BO1BQliRYWdnmBzhPzBSp4E5x-fSF9DWlAuS-M2G3_m-5zq67hmS5r4iUC55xKFduFcvYOKj8UAKOjBADWw3YBSg4pPX7UDxajLUDy8sAnyi-MABoCHdSyRJo_I_Ci2jgc2klPx_lRs7cPX7YPVJa0wo-DXAu4tHD3US57HArIseS_KJ5aQD2wmdkccW62cO4zgK_hElqTuY-iQtFptiS-KAlTR0WRocT4GFkVM_Nx2E9YKNqwKe3a5PBmblz-FKZ4KuaYCsWKoY8kMeC6GQnc6-zUc52ysljoCE5R86Jr_IauXNmFqVoL05Jzb6erYs5VeFJqFya5oLRv836aKMh_TLvOpwW0iTY'
+    'Authorization': f'Bearer {access_token}',
+    'Content-Type': 'application/json'
 }
 
+# Realizar requisição GET para obter os dados de estoque
 response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
-    # Define the file path for the JSON output in the specified folder
+    # Defina o caminho do arquivo de saída JSON
     file_path = os.path.join('E:\\Mile_Express\\Estoque', 'response.json')
 
-    # Write the JSON response to a file
+    # Salvar a resposta JSON em um arquivo
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(response.json(), f, ensure_ascii=False, indent=4)
 
-    print(f"Response saved to {file_path}")
+    print(f"Resposta salva em {file_path}")
 else:
-    print(f"Error: {response.status_code} - {response.text}")
+    print(f"Erro: {response.status_code} - {response.text}")
